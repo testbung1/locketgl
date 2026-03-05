f6bcd679962abb66201c78636af0312223a09335
Bug Fix <tf.khoindvn@gmail.com>
2026-03-05 15:39:35 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-import os
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-
-# Lấy biến môi trường
-TOKEN = os.environ.get("BOT_TOKEN")
-PORT = int(os.environ.get("PORT", 10000))
-RENDER_EXTERNAL_URL = os.environ.get("RENDER_EXTERNAL_URL")
-
-# Lệnh /start
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot đang chạy trên Render Web Service 🚀")
-
-def main():
-    app = Application.builder().token(TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-
-    # Chạy webhook cho Render
-    app.run_webhook(
-        listen="0.0.0.0",
-        port=PORT,
-        webhook_url=f"{RENDER_EXTERNAL_URL}/{TOKEN}"
-    )
+from app.bot import run_bot
 
 if __name__ == "__main__":
-    main()
+    run_bot()
