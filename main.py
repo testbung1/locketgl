ca7a62665ceb8d369fc7c67b7312f1c79429d07d
Bug Fix <tf.khoindvn@gmail.com>
2026-03-05 16:15:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import threading
+import http.server
+import socketserver
+import os
 from app.bot import run_bot
 
+# Hàm để chạy Web Server ảo
+def run_dummy_server():
+    PORT = int(os.environ.get("PORT", 10000))
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
+        print(f"Dummy server đang chạy tại port {PORT}")
+        httpd.serve_forever()
+
 if __name__ == "__main__":
+    # Chạy Web Server ở một luồng (thread) riêng
+    threading.Thread(target=run_dummy_server, daemon=True).start()
+    
+    # Chạy Bot Telegram chính
     run_bot()
